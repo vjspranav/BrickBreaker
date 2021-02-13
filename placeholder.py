207174dba066c8eed0983f026a0f0588e6f5ee31
vjspranav <pranavasri@live.in>
2021-02-14 04:16:12 +0530

    Give a bg to grid

--- placeholder.py
+++ placeholder.py
@@ -1,3 +1,8 @@
+from colorama import init, Fore, Back
+
+init(autoreset=True)
+
+
 class Placeholder:
     def __init__(self):
         self.object = None
@@ -8,12 +13,12 @@ class Placeholder:
     def add_brick(self, obj):
         self.object = obj
         self.has_object = True
-        self.has_brick =True
+        self.has_brick = True
 
     def add_power_up(self, obj):
         self.object = obj
         self.has_object = True
-        self.has_power_up =True
+        self.has_power_up = True
 
     def remove_object(self):
         self.object = None
@@ -21,8 +26,7 @@ class Placeholder:
         self.has_brick = False
         self.has_power_up = False
 
-
     def __str__(self):
         if self.has_object:
             return self.object
-        return "  *  "
+        return Back.LIGHTWHITE_EX + "  *  "
