aff50457c29563187b531ff05e1756ba3d94347e
vjspranav <pranavasri@live.in>
2021-02-14 03:04:52 +0530

    Improvise Class structure

--- placeholder.py
+++ placeholder.py
@@ -2,14 +2,25 @@ class Placeholder:
     def __init__(self):
         self.object = None
         self.has_object = False
+        self.has_brick = False
+        self.has_power_up = False
 
-    def add_object(self, obj):
+    def add_brick(self, obj):
         self.object = obj
         self.has_object = True
+        self.has_brick =True
+
+    def add_power_up(self, obj):
+        self.object = obj
+        self.has_object = True
+        self.has_power_up =True
 
     def remove_object(self):
         self.object = None
         self.has_object = False
+        self.has_brick = False
+        self.has_power_up = False
+
 
     def __str__(self):
         if self.has_object:
