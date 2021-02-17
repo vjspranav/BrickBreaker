d353fb72cdf18c4514c9a174cf3f1076c0aaa529
vjspranav <pranavasri@live.in>
2021-02-18 02:24:27 +0530

    Handle output issues on thread

--- get.py
+++ get.py
@@ -2,7 +2,7 @@
 import sys
 import termios
 import tty
-import signal
+import fcntl
 
 
 class __Get:
@@ -12,12 +12,14 @@ class __Get:
         """Defining __call__."""
         fd = sys.stdin.fileno()
         old_settings = termios.tcgetattr(fd)
+        old_flags = fcntl.fcntl(fd, fcntl.F_GETFL)
         try:
             tty.setraw(sys.stdin.fileno())
             ch = sys.stdin.read(1)
             if ch == '\x03':
                 exit()
         finally:
+            fcntl.fcntl(fd, fcntl.F_SETFL, old_flags)
             termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
         return ch
 
